ad39dc294fa38ed23b38c763975255e300a5ac85
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-10-10 13:51:38 -0300

    #2 - Utilização de dados da API de quotes

---
+++ script.js
@@ -0,0 +1,20 @@
+let apiQuotes = [];
+
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch(error) {
+        alert(error)
+    }
+}
+
+getQuotes();
