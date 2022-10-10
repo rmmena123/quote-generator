3c0a5d3b20e2d4d4c91bd96c8f4ffadbedda607c
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-10-10 14:51:03 -0300

    #3 - Geração de quotes, funcionalidade botões New Quote e Twitter

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 async function getQuotes() {
@@ -17,4 +36,15 @@ async function getQuotes() {
     }
 }
 
+function twwetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', twwetQuote);
+
+// on Load
 getQuotes();
